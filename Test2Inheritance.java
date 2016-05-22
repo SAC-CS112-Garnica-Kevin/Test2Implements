2910b08c32a2f5798dc56f88cc17e3618df218af
SAC-CS112-Garnica-Kevin <garnic18@gmail.com>
2016-05-21 21:46:35 -0700

    Add files via upload

---
+++ Test2Inheritance.java
@@ -0,0 +1,80 @@
+
+package test2inheritance;
+import java.util.Scanner;
+
+public class Test2Inheritance {
+
+    
+    public static void main(String[] args) 
+    {
+        int numberOfDie;
+        int cycles;
+        double totalDistance; 
+        String userRestarting; 
+        boolean restart;
+        
+        Scanner scan = new Scanner(System.in); 
+        
+        Drive[]  About = new Drive [3];
+        
+        truck Truck = new truck();
+        van Van = new van();
+        sportscar Sportscar = new sportscar();
+        
+        
+        System.out.print("Please input the number of Dice you want to throw: ");
+        numberOfDie = scan.nextInt();
+        System.out.println(" ");
+        
+        
+
+        About[0] = Truck;
+        About[1] = Van;
+        About[2] = Sportscar;
+        
+        do 
+        {
+            for (Drive X: About)
+            {
+                double TotalCycles = (X.Throw(numberOfDie));
+                System.out.println("The Cycle for this Run will be " + TotalCycles);
+                X.About(X.DistanceTraveled(TotalCycles));
+                System.out.println(" ");
+
+            }
+            
+            System.out.print("Do You Wish to Run Another Cycle: ");
+            userRestarting = scan.next();
+            System.out.println(" ");
+            
+            while (true)
+            {
+                if (userRestarting.equalsIgnoreCase("Yes"))
+                {
+                    restart = true;
+                    break;
+                }
+                else if (userRestarting.equalsIgnoreCase("no"))
+                {
+                    restart = false; 
+                    break;
+                }
+                else
+                {
+                    System.out.println("Invalid Input Try Again");
+                    System.out.println(" ");
+                    System.out.print("Do You Wish to Run Another Cycle: ");
+                    userRestarting = scan.next();
+                    System.out.println(" ");
+                }
+            } 
+            
+        }while (restart);
+        
+       
+        
+        
+        
+    }
+    
+}
---
+++ truck.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test2inheritance;
+
+import java.util.Random;
+
+/**
+ *
+ * @author owner
+ */
+public class truck implements Drive 
+{
+    private double gear = 6.0;
+    private double totalDistance;
+    private double dieNumbers;
+    private double lowestValue = 1;
+    private double highestValue = 6;
+    private double totalCycles;
+    private String discription;
+    
+    @Override 
+    public double Throw(double dices)
+    {
+        dieNumbers = dices;
+        lowestValue = lowestValue * dieNumbers;
+        highestValue = highestValue * dieNumbers;
+        Random rand = new Random();
+        double cycles = lowestValue + (int)(Math.random() * ((highestValue - lowestValue) + 1));
+        return cycles;
+    }
+    
+    @Override 
+    public double DistanceTraveled(double totalCycles)
+    { 
+        for (double cycles = 0; cycles <= totalCycles; cycles++)
+        {
+            if (totalCycles == 0)
+            {
+                totalDistance = 0; 
+            }
+            else if (cycles <= gear)
+            {
+
+                totalDistance += (cycles * 10)/60.00;
+                this.totalDistance += (cycles * 10)/60.00;
+            }
+            else if (cycles > gear)
+            {
+                totalDistance += (gear * 10)/60.00;
+                this.totalDistance += (cycles * 10)/60.00;
+            }
+            
+        }
+        this.totalCycles = totalCycles;
+        return totalDistance;
+    }
+    
+    @Override 
+    public void About(double totalDistance)
+    {
+        System.out.println("Trucky the Black 2009 Toyota Tacoma, has traveled " + totalDistance + " Miles");   
+    }
+}
